50d0c8bc725d278d963e08b719367e411b05e2e7
Kiwon Kim <kiwon@elicer.com>
2022-09-17 19:38:35 +0900

    feat: add function comment

--- server/schema.js
+++ server/schema.js
@@ -9,6 +9,7 @@ const { companies, users } = require('./datas');
 
 const CompayType = new GraphQLObjectType({
   name: 'Company',
+  // 아래에 정의된 UserType을 사용하기 때문에 함수로 감싼다.
   fields: () => ({
     id: { type: GraphQLString },
     name: { type: GraphQLString },
